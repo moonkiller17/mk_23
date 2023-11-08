95087a9ef6ab3e9c2a81a264fef7ed9a02f50935
Вадим Мохов <vad.mohov.18@gmail.com>
2023-11-08 13:18:24 +0300

    writting first program

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,17 @@
+#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/rcc.h>
+
 int main () {
 
+    // Разморозка светодиодов
+    rcc_periph_clock_enable(RCC_GPIOE);
+
+    gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO9);
+
     while (true) {
+        gpio_toggle(GPIOE, GPIO9);
+
+        for (volatile uint32_t i = 0; i < 500'000; ++i);
 
     }
 }
