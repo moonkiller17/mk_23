9dec5e4d85eb3ee63eb33bcd72d5097f73060a9e
Вадим Мохов <vad.mohov.18@gmail.com>
2023-11-14 13:13:33 +0300

    add timer

--- src/main.cpp
+++ src/main.cpp
@@ -5,15 +5,23 @@ int main () {
 
     rcc_clock_setup_pll(&rcc_hsi_configs[RCC_CLOCK_HSI_64MHZ]);
 
-    // Разморозка светодиодов
-    rcc_periph_clock_enable(RCC_GPIOE);
+    // Настройка таймера
+    rcc_periph_clock_enable(RCC_TIM6);
+    timer_set_prescaler(TIM6, 36000 - 1);
+    timer_set_period(TIM6, 1000 - 1);
 
+    timer_enable_counter(TIM6);
+
+    // Настройка порта ввода-вывода
+    rcc_periph_clock_enable(RCC_GPIOE);
     gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO9);
 
     while (true) {
-        gpio_toggle(GPIOE, GPIO9);
-
-        for (volatile uint32_t i = 0; i < 500'000; ++i);
-
+        if (timer_get_counter(TIM6) < 500) {
+            gpio_set(GPIOE, GPIO9);
+        }
+        else {
+            gpio_clear(GPIOE, GPIO9);
+        }
     }
 }
