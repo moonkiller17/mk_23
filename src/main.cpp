c3ae0022d25f68e59a636d5683ea7b3673eb9ec3
Вадим Мохов <vad.mohov.18@gmail.com>
2023-11-28 12:56:00 +0300

    upgrade program "alarm-timer"

--- src/main.cpp
+++ src/main.cpp
@@ -39,12 +39,42 @@ void blink_LED() {
 }
 
 
+void tim6_dac_isr() {
+
+    timer_clear_flag(TIM6, TIM_SR_UIF);
+    gpio_toggle(GPIOE, GPIO11);
+}
+
+
+void timer_1_setup() {
+
+    // Настройка таймера
+    rcc_periph_clock_enable(RCC_GPIOE);
+    rcc_periph_clock_enable(RCC_TIM1);
+    
+    gpio_mode_setup(GPIOE, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO14);
+    gpio_set_af(GPIOE, GPIO_AF2, GPIO14);
+
+    timer_set_prescaler(TIM1, rcc_get_timer_clk_freq(TIM1) / PERIOD_MS - 1);
+    timer_set_period(TIM1, PERIOD_MS - 1);
+
+    timer_set_oc_value(TIM1, TIM_OC4, PERIOD_MS / 2);
+    timer_set_oc_mode(TIM1, TIM_OC4, TIM_OCM_PWM1);
+    timer_enable_oc_output(TIM1, TIM_OC4);
+
+    timer_enable_break_main_output(TIM1);
+
+    timer_enable_counter(TIM1);
+}
+
+
 int main () {
 
     // Настройка тактов
     rcc_clock_setup_pll(&rcc_hsi_configs[RCC_CLOCK_HSI_64MHZ]);
 
     timer_setup();
+    timer_1_setup();
 
     gpio_setup();
 
@@ -52,10 +82,4 @@ int main () {
 
         blink_LED();
     }
-}
-
-void tim6_dac_isr() {
-
-    timer_clear_flag(TIM6, TIM_SR_UIF);
-    gpio_toggle(GPIOE, GPIO11);
 }
